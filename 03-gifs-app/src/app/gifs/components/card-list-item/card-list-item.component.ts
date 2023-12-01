119b61370627abca2cd875925be7cedbe412b0f8
Farney Jiménez <farney9@gmail.com>
2023-11-30 21:03:31 -0500

    refactor card list add card list item component

--- 03-gifs-app/src/app/gifs/components/card-list-item/card-list-item.component.ts
+++ 03-gifs-app/src/app/gifs/components/card-list-item/card-list-item.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interface';
 
 @Component({
   selector: 'gifs-card-list-item',
   templateUrl: './card-list-item.component.html',
   styleUrls: ['./card-list-item.component.sass']
 })
-export class CardListItemComponent {
+export class CardListItemComponent implements OnInit {
+  @Input() gif!: Gif;
+  ngOnInit(): void {
+    if (!this.gif) throw new Error('Gif property is required');
+  }
+
+
+
 
 }
