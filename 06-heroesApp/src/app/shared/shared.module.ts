a14aa8408bebd06329a13b0682500136f12673c7
Farney Jiménez <farney9@gmail.com>
2024-01-06 11:58:45 -0500

    add material module & page

--- 06-heroesApp/src/app/shared/shared.module.ts
+++ 06-heroesApp/src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
+  declarations: [
+    NotFoundPageComponent
+  ],
+  exports: [
+    NotFoundPageComponent
   ]
 })
 export class SharedModule { }
