a9536752a66f0a0da1d24583a9ecc81552cf66e4
Farney Jiménez <farney9@gmail.com>
2024-02-21 17:08:25 -0500

    default Form value & reset value

--- 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 = {
+  name: 'RTX5090',
+  price: 2500,
+  inStorage: 6
+}
 
 @Component({
   selector: 'app-basic-page',
   templateUrl: './basic-page.component.html',
   styles: ``
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
 
 
 
@@ -24,10 +30,15 @@ export class BasicPageComponent {
 
   constructor( private fb: FormBuilder) {}
 
+  ngOnInit() {
+this.basicForm.reset(rtx5090);
+  }
+
   onSave() {
     if (this.basicForm.invalid) return;
     console.log(this.basicForm.value);
 
+    this.basicForm.reset({price: 10, inStorage: 0});
   }
 
 
