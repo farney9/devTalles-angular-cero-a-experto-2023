fb5948912513ac3e376d291d41df86502eac5bc3
Farney Jiménez <farney9@gmail.com>
2024-02-21 16:57:15 -0500

    Forms validator

--- 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class BasicPageComponent {
+
+
+
+/*   basicForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(0),
+    inStorage: new FormControl(0),
+  }) */
+
+  basicForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]]
+  })
+
+  constructor( private fb: FormBuilder) {}
+
+  onSave() {
+    if (this.basicForm.invalid) return;
+    console.log(this.basicForm.value);
+
+  }
+
+
 
 }
