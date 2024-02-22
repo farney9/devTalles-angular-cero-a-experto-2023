5bc7808964e66727e1e2b6b985f064240c1b349d
Farney Jiménez <farney9@gmail.com>
2024-02-22 12:45:07 -0500

    revome elemensts of FormArray

--- 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -12,15 +12,13 @@ const rtx5090 = {
   templateUrl: './basic-page.component.html',
   styles: ``
 })
-export class BasicPageComponent implements OnInit{
+export class BasicPageComponent implements OnInit {
 
-
-
-/*   basicForm: FormGroup = new FormGroup({
-    name: new FormControl(''),
-    price: new FormControl(0),
-    inStorage: new FormControl(0),
-  }) */
+  /*   basicForm: FormGroup = new FormGroup({
+      name: new FormControl(''),
+      price: new FormControl(0),
+      inStorage: new FormControl(0),
+    }) */
 
   basicForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
@@ -28,19 +26,39 @@ export class BasicPageComponent implements OnInit{
     inStorage: [0, [Validators.required, Validators.min(0)]]
   })
 
-  constructor( private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-this.basicForm.reset(rtx5090);
+    // this.basicForm.reset(rtx5090);
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.basicForm.controls[field]) return null;
+
+    const errors = this.basicForm.controls[field].errors || {}
+
+    for (const key of Object.keys(errors)) {
+      switch( key ) {
+        case 'required': return 'This field is required.'
+        case 'minlength': return `Must have minimum ${errors['minlength'].requiredLength} characters.`
+      }
+    }
+
+    return null;
+  }
+
+  isvalidField(field: string): boolean | null {
+    return this.basicForm.controls[field].errors
+      && this.basicForm.controls[field].touched;
   }
 
   onSave() {
-    if (this.basicForm.invalid) return;
+    if (this.basicForm.invalid) {
+      this.basicForm.markAllAsTouched();
+      return
+    }
     console.log(this.basicForm.value);
 
-    this.basicForm.reset({price: 10, inStorage: 0});
+    this.basicForm.reset({ price: 0, inStorage: 0 });
   }
-
-
-
 }
--- 07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-page',
@@ -6,5 +7,59 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class DynamicPageComponent {
+
+  dynamicForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ]),
+  })
+
+  constructor(private fb: FormBuilder) { }
+
+  get favoriteGames() {
+    return this.dynamicForm.get('favoriteGames') as FormArray;
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.dynamicForm.controls[field]) return null;
+
+    const errors = this.dynamicForm.controls[field].errors || {}
+
+    for (const key of Object.keys(errors)) {
+      switch( key ) {
+        case 'required': return 'This field is required.'
+        case 'minlength': return `Must have minimum ${errors['minlength'].requiredLength} characters.`
+      }
+    }
+
+    return null;
+  }
+
+  isvalidFieldinArray(formArray: FormArray, idx: number): boolean | null {
+    return formArray.controls[idx].errors
+        && formArray.controls[idx].touched;
+  }
+
+  isvalidField(field: string): boolean | null {
+    return this.dynamicForm.controls[field].errors
+      && this.dynamicForm.controls[field].touched;
+  }
+
+  onDeletefavorite(idx: number) {
+    this.favoriteGames.removeAt(idx);
+  }
+
+
+  onSubmit() {
+    if (this.dynamicForm.invalid) {
+      this.dynamicForm.markAllAsTouched();
+      return
+    }
+    console.log(this.dynamicForm.value);
+
+    this.dynamicForm.reset();
+  }
 
 }
