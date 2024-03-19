f63b223f7c499fe8a9eed5d24378572abec19a07
Farney Jiménez <farney9@gmail.com>
2024-03-19 15:46:38 -0500

    Diseño ZoomIn ZoomOut

--- 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'maplibre-gl';
 
 @Component({
   selector: 'app-zoom-range-page',
   templateUrl: './zoom-range-page.component.html',
   styleUrl: './zoom-range-page.component.sass'
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit {
+
+  @ViewChild('map') divMap?: ElementRef;
+
+  ngAfterViewInit(): void {
+
+    if (!this.divMap) throw 'El elemento HTML no fue encontrado';
+
+    const map = new Map({
+      container: this.divMap?.nativeElement,
+      style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
+      center: [-75.55, 6.22], // starting position [lng, lat]
+      // center: [-75.5599231, 6.2251537], // starting position [lng, lat]
+      zoom: 9 // starting zoom
+    });
+
+  }
 
 }
