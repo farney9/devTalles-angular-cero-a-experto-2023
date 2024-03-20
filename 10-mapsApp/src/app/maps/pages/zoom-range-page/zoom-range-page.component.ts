d594fb7927f1e47d66ee70783ac183ff17aa4f89
Farney Jiménez <farney9@gmail.com>
2024-03-20 13:16:28 -0500

    add methos ZoomIn ZoomOut OnZoomChange

--- 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ 10-mapsApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -10,18 +10,48 @@ export class ZoomRangePageComponent implements AfterViewInit {
 
   @ViewChild('map') divMap?: ElementRef;
 
+  currentZoom = 4;
+  map?: Map;
+
   ngAfterViewInit(): void {
 
     if (!this.divMap) throw 'El elemento HTML no fue encontrado';
 
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap?.nativeElement,
       style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
       center: [-75.55, 6.22], // starting position [lng, lat]
       // center: [-75.5599231, 6.2251537], // starting position [lng, lat]
-      zoom: 9 // starting zoom
+      zoom: this.currentZoom // starting zoom
+    });
+    this.maplisteners();
+  }
+
+  maplisteners() {
+    if (!this.map) throw 'Mapa no inicializado';
+    this.map?.on('zoom', (event) => {
+      this.currentZoom = this.map?.getZoom() ?? 0;
     });
 
+    this.map?.on('zoomend', (event) => {
+      if (this.map!.getZoom() < 7) return
+      this.map!.zoomTo(7);
+    });
+  }
+
+  onZoomChange(event: any) {
+    if (!this.map) throw 'Mapa no inicializado';
+    const zoom = +event.target.value;
+    this.map?.zoomTo(zoom);
+  }
+
+  zoomIn() {
+    if (!this.map) throw 'Mapa no inicializado';
+    this.map?.zoomIn();
+  }
+  zoomOut() {
+    if (!this.map) throw 'Mapa no inicializado';
+    this.map?.zoomOut();
   }
 
 }
