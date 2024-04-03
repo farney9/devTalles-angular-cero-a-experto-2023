6dd80f27f8b70f0064af0801d48ff7b4bd15f2bb
Farney Jiménez <farney9@gmail.com>
2024-04-02 23:00:57 -0500

    Add AuthService

--- 13-authApp/src/app/auth/services/auth.service.ts
+++ 13-authApp/src/app/auth/services/auth.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject, signal } from '@angular/core';
 
+
+import { environment } from '../../../environments/environment.development';
+import { Observable, of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private readonly url: string = environment.apiUrl;
+  private http = inject(HttpClient);
+
+  private _currentUser = signal<User | null>(null);
+  private _authStatus = signal<AuthStatus>(false);
+
   constructor() { }
+
+  login( email: string, password: string): Observable<boolean> {
+    this.http.post<User>(`${this.url}/login`, { email, password })
+
+
+    return of(true);
+  }
+
 }
