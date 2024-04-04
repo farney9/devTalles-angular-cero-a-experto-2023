21a94e036ab1dde039c5b316494a0f90c1c6c43e
Farney Jiménez <farney9@gmail.com>
2024-04-04 01:46:26 -0500

    perform project structure

---
+++ my-dashboard/src/app/dashboard/dashboard-page.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard-page.component.html',
+  styles: ``
+})
+export class DashboardPageComponent {
+
+}
---
+++ my-dashboard/src/app/dashboard/pages/user-list-page/user-list-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  templateUrl: './user-list-page.component.html',
+  styles: ``
+})
+export class UserListPageComponent {
+
+}
