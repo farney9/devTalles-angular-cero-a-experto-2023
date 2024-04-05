29c31900afb811c43db0969f2094b6921f14d3d4
Farney Jiménez <farney9@gmail.com>
2024-04-04 20:47:47 -0500

    Control Flow Directive  @for

--- my-dashboard/src/app/dashboard/pages/control-flow-page/control-flow-page.component.ts
+++ my-dashboard/src/app/dashboard/pages/control-flow-page/control-flow-page.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
 type Grade = 'A' | 'B' | 'F';
@@ -5,17 +6,32 @@ type Grade = 'A' | 'B' | 'F';
 @Component({
   selector: 'app-control-flow-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './control-flow-page.component.html',
   styles: ``
 })
 export default class ControlFlowPageComponent {
 
-  showContent = signal(false);
+  frameworks = signal(['Angular', 'Svelte', 'Vue', 'Qwik']);
+  frameworks2 = signal([]);
   grade = signal<Grade>('A');
+  showContent = signal(false);
 
   toggleContent() {
     this.showContent.update(value => !value);
   }
+
+/* if (first || last) {
+      return 'text-blue-800 font-bold';
+    } else if (even) {
+      return 'text-red-800 font-bold';
+    } else if (odd) {
+      return 'text-green-800 font-bold';
+    }
+    return '';
+  } */
+
+  
+
 
 }
