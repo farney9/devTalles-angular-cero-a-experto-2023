ef3733f670177b7726f57a01bdd50149b880ca28
Farney Jiménez <farney9@gmail.com>
2024-04-06 01:58:32 -0500

    @defer Triggers

--- my-dashboard/src/app/dashboard/pages/defer-options-page/defer-options-page.component.ts
+++ my-dashboard/src/app/dashboard/pages/defer-options-page/defer-options-page.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
+import { HeavyLoadersFastComponent } from '../../../shared/heavy-loaders/heavy-loaders-fast.component';
 
 @Component({
   selector: 'app-defer-options-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TitleComponent, HeavyLoadersFastComponent],
   templateUrl: './defer-options-page.component.html',
   styles: ``
 })
--- my-dashboard/src/app/dashboard/pages/defer-views-page/defer-views-page.component.ts
+++ my-dashboard/src/app/dashboard/pages/defer-views-page/defer-views-page.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { HeavyLoadersSlowComponent } from '@shared/heavy-loaders/heavy-loaders-slow.component';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-defer-views-page',
   standalone: true,
-  imports: [HeavyLoadersSlowComponent],
+  imports: [HeavyLoadersSlowComponent, TitleComponent],
   templateUrl: './defer-views-page.component.html',
   styles: ``
 })
 export default class DeferViewsPageComponent {
 
-  
+
 
 }
