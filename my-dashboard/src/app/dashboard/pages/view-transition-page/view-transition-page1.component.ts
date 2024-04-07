228e95d0f052318fb7eaa5f99c8869209f557cb3
Farney Jiménez <farney9@gmail.com>
2024-04-07 18:28:43 -0500

    View Transitions API

---
+++ my-dashboard/src/app/dashboard/pages/view-transition-page/view-transition-page1.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
+
+@Component({
+  selector: 'app-view-transition-page',
+  standalone: true,
+  imports: [CommonModule, TitleComponent],
+  template: `
+  <app-title title="View Transition Page 1" />
+
+  <section class="flex justify-start">
+
+  <img srcset="//picsum.photos/id/237/300/200.webp" width="300" height="200" alt="PicSum Photos">
+
+  <div class="bg-blue-500 w-56 h-56">
+
+  </div>
+  </section>
+
+  `,
+  styles: ``
+})
+export default class ViewTransitionPageComponent {
+
+}
