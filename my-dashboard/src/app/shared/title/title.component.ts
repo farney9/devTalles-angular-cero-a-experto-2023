6b54bb2f15fb1e39b8f5faeebd675344180c1c77
Farney Jiménez <farney9@gmail.com>
2024-04-08 11:51:08 -0500

    solucion al problema de '- false' en app-title - styles

--- my-dashboard/src/app/shared/title/title.component.ts
+++ my-dashboard/src/app/shared/title/title.component.ts
@@ -18,4 +18,13 @@ export class TitleComponent {
 
   @Input({ required: true }) title!: string;
   @Input({ transform: booleanAttribute }) withShadow: boolean = false;
+
+  /*
+  La propiedad withShadow es un atributo de entrada (@Input) en un componente de Angular. Permite que un valor booleano se pase al componente desde su componente padre. En este caso, se establece un valor predeterminado de false para withShadow en caso de que no se proporcione ningún valor desde el componente padre.
+
+  El atributo transform utilizado en @Input({ transform: booleanAttribute }) es un decorador que se utiliza para modificar el comportamiento predeterminado de cómo se pasa el valor al componente. En este caso, booleanAttribute es una función que transforma el valor pasado al componente en un valor booleano. Esto significa que si se pasa un valor no booleano, como una cadena o un número, se convertirá automáticamente en un valor booleano antes de asignarse a withShadow.
+
+  En resumen, esta propiedad permite que el componente reciba un valor booleano desde su componente padre, y si no se proporciona ningún valor, se establece en false de forma predeterminada. Además, el decorador @Input con el atributo transform garantiza que cualquier valor no booleano se convierta en un valor booleano antes de asignarse a withShadow.
+  */
+
 }
